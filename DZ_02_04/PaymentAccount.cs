cf78152bc699aba4cbb36ddadf65357407788e37
ElNefro <131360806+ElNefro@users.noreply.github.com>
2025-04-14 12:48:01 +0500

    Update PaymentAccount.cs

--- DZ_02_04/PaymentAccount.cs
+++ DZ_02_04/PaymentAccount.cs
@@ -5,7 +5,7 @@ using System.Runtime.Serialization;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ConsoleApp13
+namespace DZ_02_04
 {
     public class PaymentAccount : ISerializable
     {
